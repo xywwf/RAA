dc956c2f44631e2906f64f55111e3f12f1efbb8c
Lee02 <809262979@qq.com>
2018-03-17 14:16:57 +0800

    update file dir
    修改和发布部分文件.

--- Cef/normalize-line-endings.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-"%~dp0\tools\xifixeol" "%~dp0\" *.cs lf utf8
-"%~dp0\tools\xifixeol" "%~dp0\" *.csproj lf utf8
-"%~dp0\tools\xifixeol" "%~dp0\" *.h lf ascii
-"%~dp0\tools\xifixeol" "%~dp0\" *.py lf ascii
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RAA
 this text that RAA for wows technical analysis
+
+Aria2 for libaria2
+aria2-Edit_C as for to ttps://github.com/jashking/aria2
+aria2-1.33.1 is my edit aria2 for libaria2
